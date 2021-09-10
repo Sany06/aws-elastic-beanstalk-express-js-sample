bd0777432237a576a9019718d616b5fea4419f6a
Sany06 <90442685+Sany06@users.noreply.github.com>
2021-09-10 20:32:01 +0530

    new

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('happy new year'));
+app.get('/', (req, res) => res.send('happy new years'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
